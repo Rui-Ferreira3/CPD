cab3eb106e413b926b78e349edcca05142d00825
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-12 21:08:24 +0100

    test pack

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -45,10 +45,10 @@ int main(int argc, char *argv[]) {
 
         // Send the buffer to the receiving process
         /*for loop to send to all processes*/
-        MPI_Send(buffer, bufSize, MPI_PACKED, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&buffer, bufSize, MPI_PACKED, 1, 0, MPI_COMM_WORLD);
     }else {
-        MPI_Recv(buffer, bufSize, MPI_PACKED, 0, 0, MPI_COMM_WORLD);
-        MPI_Unpack(buffer, bufSize, &position, &buffer[0], sizeof(QueueElem), MPI_BYTE, MPI_COMM_WORLD);
+        MPI_Recv(&buffer, bufSize, MPI_PACKED, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Unpack(buffer, bufSize, &position, &buffer[0], bufSize, MPI_BYTE, MPI_COMM_WORLD);
         myElem = (QueueElem)buffer[0];
         printQueueElem(myElem);
     }
