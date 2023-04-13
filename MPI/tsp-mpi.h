73aec581fe8ff9e924325590cc0e2b3e2c09c70d
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 22:16:59 +0100

    load balancing

--- MPI/tsp-mpi.h
+++ MPI/tsp-mpi.h
@@ -20,7 +20,7 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
 void send_element(int dest, int tag, QueueElem elem, MPI_Datatype elem_type);
 QueueElem recv_element(int source, int tag, MPI_Datatype elem_type);
 void create_children(QueueElem &myElem, PriorityQueue<QueueElem> &myQueue, vector<pair<double,double>> &mins);
-void get_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Datatype elem_type)
+void get_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Datatype elem_type);
 void split_work(int num_processes, PriorityQueue<QueueElem> &startQueue);
 void send_BestTourCost(int rank);
 void update_BestTour(int rank, vector <int> &BestTour);
