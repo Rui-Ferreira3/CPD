6b53f5d1232769dee0f67d1ac54b385ff36ff6ca
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 12:18:46 +0100

    run tests

--- MPI/tsp-mpi.h
+++ MPI/tsp-mpi.h
@@ -21,6 +21,8 @@ void send_element(int dest, int tag, QueueElem elem, MPI_Datatype elem_type);
 QueueElem recv_element(int tag, MPI_Datatype elem_type);
 void create_children(QueueElem &myElem, PriorityQueue<QueueElem> &myQueue, vector<pair<double,double>> &mins);
 void split_work(int num_processes, PriorityQueue<QueueElem> &startQueue);
+void send_BestTourCost(int rank);
+void update_BestTour(int rank, vector <int> &BestTour);
 void redistribute_elements(PriorityQueue<QueueElem> &myQueue, int rank);
 vector<pair<double,double>> get_mins();
 double initialLB(vector<pair<double,double>> &mins);
