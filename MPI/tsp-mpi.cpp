bbaa2dac837b39fd681c3b033cb1b303c13906d3
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 16:12:30 +0100

    send/recv best tour

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -341,15 +341,15 @@ void redistribute_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Data
     send_element(dest, 2, myQueue.pop(), elem_type);
     printf("Elements sent in %d\n", rank);
 
-    int flag;
-    MPI_Iprobe(source, 2, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
-    if(flag) {
+    // int flag;
+    // MPI_Iprobe(source, 2, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+    // if(flag) {
         for(int i=0; i<NUM_SWAPS; i++) {
             QueueElem newElem = recv_element(source, 2, elem_type);
             myQueue.push(newElem);
         }
         printf("Elements received in %d\n", rank);
-    }
+    // }
 }
 
 vector<pair<double,double>> get_mins() {
