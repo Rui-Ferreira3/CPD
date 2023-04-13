a44053902d6d99ae1c8030cf74b7445932a0e3cb
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 16:48:09 +0100

    send/recv best tour

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -267,8 +267,8 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
         // cnt++;
         
         int size = myQueue.size();
-        MPI_Reduce(&size, &flag, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        printf("Rank %d Size %d\n", rank, flag);
+        MPI_Reduce(&size, &flag, 0, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        printf("Rank %d Flag %d\n", rank, flag);
     }
 
     return make_pair(BestTour, BestTourCost);
