ec568e50c08b609f66916469f6a0897207c72f60
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-14 00:32:02 +0100

    load balancing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -288,11 +288,15 @@ void get_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Datatype elem
     int flag;
     
     // printf("Process %d queue receiving elements. Queue size is %d\n", rank, myQueue.size());
-    for(int i=0; i<NUM_SWAPS; i++) {
-        MPI_Iprobe(source, 2, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
-        if(flag) {
-            QueueElem newElem = recv_element(source, 2, elem_type);
-            myQueue.push(newElem);
+    for(int j=0; j<num_processes; j++) {
+        if(j!=rank) {
+            for(int i=0; i<NUM_SWAPS; i++) {
+                MPI_Iprobe(j, 2, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+                if(flag) {
+                    QueueElem newElem = recv_element(j, 2, elem_type);
+                    myQueue.push(newElem);
+                }
+            }
         }
     }
     // printf("Process %d queue finished receiving elements. New queue size is %d\n", rank, myQueue.size());
@@ -356,10 +360,14 @@ void redistribute_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Data
         dest = rank-1;
     }
 
-    if(myQueue.size() >= NUM_ITERATIONS) {
+    if(myQueue.size() >= num_processes*NUM_SWAPS) {
         // printf("Process %d queue sending elements. Queue size is %d\n", rank, myQueue.size());
-        for(int i=0; i<NUM_SWAPS; i++) {
-            send_element(dest, 2, myQueue.pop(), elem_type);
+        for(int j=0; j<num_processes; j++) {
+            if(j!=rank) {
+                for(int i=0; i<NUM_SWAPS; i++) {
+                    send_element(j, 2, myQueue.pop(), elem_type);
+                }
+            }
         }
         // printf("Process %d queue finished sending elements. New queue size is %d\n", rank, myQueue.size());
     }
