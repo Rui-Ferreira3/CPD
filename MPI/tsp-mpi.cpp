19e9208af825558d4aaafa604da852883eaf86cd
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 14:24:49 +0100

    send/recv best tour

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -302,13 +302,10 @@ void update_BestTour(int rank, vector <int> &BestTour, MPI_Status &status, MPI_R
             double newBest = BestTourCost;
             MPI_Irecv(&newBest, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &request);
             int flag;
-            MPI_Test(&request, &flag, &status);
-            if(flag) {
-                if(newBest < BestTourCost) {
-                    BestTourCost = newBest;
-                    BestTour = {0};
-                }
-                MPI_Request_free(&request);
+            MPI_Wait(&request, &status);
+            if(newBest < BestTourCost) {
+                BestTourCost = newBest;
+                BestTour = {0};
             }
         }
     }
