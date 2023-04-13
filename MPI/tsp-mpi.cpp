927a7362e43d93a2e89a50c152a1f265711add31
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-13 22:28:18 +0100

    load balancing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -285,11 +285,12 @@ void get_elements(PriorityQueue<QueueElem> &myQueue, int rank, MPI_Datatype elem
     int flag;
     MPI_Iprobe(source, 2, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
     if(flag) {
+        printf("Process %d queue receiving elements. Queue size is %d\n", rank, myQueue.size());
         for(int i=0; i<NUM_SWAPS; i++) {
             QueueElem newElem = recv_element(source, 2, elem_type);
             myQueue.push(newElem);
         }
-        printf("Received elements in process %d\n", rank);
+        printf("Process %d queue finished receiving elements. New queue size is %d\n", rank, myQueue.size());
     }
 }
 
