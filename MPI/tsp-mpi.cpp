d4b50483eefdcdfcd062b9a99adfebd2650f9c54
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-14 20:58:59 +0100

    somthing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -297,7 +297,7 @@ QueueElem recv_element(int source, int tag, MPI_Datatype elem_type) {
     int size = (numCities+1)*sizeof(int) + 2*sizeof(int) + 2*sizeof(double), pos = 0;
     printf("Size: %d\n", size);
     char buffer[size] = {};
-    MPI_Recv(buffer[0], size, MPI_CHAR, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&buffer[0], size, MPI_CHAR, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     printf("Received element: %.*s\n", size, buffer);
     for(int i=0; i<numCities+1; i++) {
         memcpy(&elem.tour[i], &buffer[pos], sizeof(int));
