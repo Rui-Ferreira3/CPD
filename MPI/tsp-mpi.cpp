032958e85023915f3676f9cf89097e7e8bbd3df9
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-14 21:13:40 +0100

    somthing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -209,8 +209,12 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
         
         int size = myQueue.size();
         MPI_Allreduce(&size, &flag, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-        if(flag == 0)
-            print_result(BestTour, BestTourCost);
+        if(flag == 0) {
+            cout << fixed << BestCost << endl;
+            for(int i=0; i<numCities+1; i++) {
+                cout << BestTour[i] << " ";
+            }
+        }
         // printf("Total number of elements in queues is %d\n", flag);
     }
 
