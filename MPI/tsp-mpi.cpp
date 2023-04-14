2eb58034f64c67dfdde726cd86ca84dbd3423fec
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-14 03:57:16 +0100

    load balancing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -239,8 +239,8 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
     int cnt=0;
     int flag=5;
     while(flag != 0){
-        if(num_processes > 1)
-            get_elements(myQueue, rank, elem_type);
+        // if(num_processes > 1)
+        //     get_elements(myQueue, rank, elem_type);
 
         if(myQueue.size() > 0) {
             QueueElem myElem = myQueue.pop();
@@ -258,20 +258,21 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
                             BestTour = myElem.tour;
                             BestTour.push_back(0);
                             BestTourCost = myElem.cost + dist;
-                            send_BestTourCost(rank);
+                            if(num_processes > 1)
+                                send_BestTourCost(rank);
                         }
                     }
                 }else 
                     create_children(myElem, myQueue, mins);
             }
 
-            if(num_processes > 0) {
-                if(cnt > NUM_ITERATIONS) {
-                    redistribute_elements(myQueue, rank, elem_type);
-                    cnt = 0;
-                }else
-                    cnt++;
-            }
+            // if(num_processes > 0) {
+            //     if(cnt > NUM_ITERATIONS) {
+            //         redistribute_elements(myQueue, rank, elem_type);
+            //         cnt = 0;
+            //     }else
+            //         cnt++;
+            // }
             // printf("Iteration %d of rank %d\n", cnt, rank);
         }
         
