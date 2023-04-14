6ca999ddb34136ae1c979db221481ef66b709602
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-14 21:10:27 +0100

    somthing

--- MPI/tsp-mpi.cpp
+++ MPI/tsp-mpi.cpp
@@ -209,6 +209,8 @@ pair<vector <int>, double> tsp(PriorityQueue<QueueElem> &myQueue, int rank, MPI_
         
         int size = myQueue.size();
         MPI_Allreduce(&size, &flag, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        if(flag == 0)
+            print_result(BestTour, BestTourCost);
         // printf("Total number of elements in queues is %d\n", flag);
     }
 
